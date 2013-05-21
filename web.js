cad95d4cea2754db7c78d818e93ad69d0f6e4ead
jeane paul <jeanepaul@gmail.com>
2013-05-22 01:26:15 +0900

    added index.html and modified the web.js

--- web.js
+++ web.js
@@ -2,11 +2,23 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+//app.get('/', function(request, response) {
+//  response.send('Hello World!');
+//});
+//
+//var port = process.env.PORT || 4000;
+//app.listen(port, function() {
+//  console.log("Listening on " + port);
+//});
+
+app.use(express.compress());
+var oneDay = 86400000;
+
+app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
+
 
 var port = process.env.PORT || 4000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+    console.log('Listening on ' + port)
+
+})
