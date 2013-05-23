b2220199e22bb962c8c62d93b7bd99e4b3139a5f
jeane paul <jeanepaul@gmail.com>
2013-05-24 01:01:54 +0900

    added redis

--- web.js
+++ web.js
@@ -2,14 +2,14 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
-//app.get('/', function(request, response) {
-//  response.send('Hello World!');
-//});
-//
-//var port = process.env.PORT || 4000;
-//app.listen(port, function() {
-//  console.log("Listening on " + port);
-//});
+var redis = require('redis'),
+    client = redis.createClient(14396, 'pub-redis-14396.us-east-1-3.2.ec2.garantiadata.com');
+client.auth('jinpol', function(err, res) {
+    if (err) {
+        console.log('auth error ' + err)
+    } else { console.log('authorized..') }
+})
+
 
 app.use(express.compress());
 var oneDay = 86400000;
