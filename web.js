db0afccadff19412105aec32169da4ae9329fb1b
jeane paul <jeanepaul@gmail.com>
2013-05-29 08:28:13 +0900

    enhancing redis

--- web.js
+++ web.js
@@ -4,14 +4,14 @@ var app = require('express')(),
 
 
 var redis = require('redis'),
-//    client = redis.createClient(14396, 'pub-redis-14396.us-east-1-3.2.ec2.garantiadata.com');
-//    client.auth('jinpol', function(err, res) {
-//        if (err) {
-//            console.log('auth error ' + err)
-//        } else { console.log('authorized..') }
-//    });
+    client = redis.createClient(14396, 'pub-redis-14396.us-east-1-3.2.ec2.garantiadata.com');
+    client.auth('jinpol', function(err, res) {
+        if (err) {
+            console.log('auth error ' + err)
+        } else { console.log('authorized..') }
+    });
 
-    client = redis.createClient()
+//    client = redis.createClient()
 
 app.get(/^(.+)$/, function(req, res) {
     res.sendfile('public/' + req.params[0]);
@@ -37,46 +37,65 @@ io.sockets.on('connection', function(socket) {
     });
 
     socket.on('userRegistration', function(data) {
-        client.lrange(queryConstruct('users', 'id'), 0, -1, function(err, usersId) {
-            for(var i = 0, c = 1, keyFound = []; i < usersId.length; i++) {
-                client.hget([queryConstruct('users', usersId[i]), 'name'], function(err, user) {
-                    if (user == data['name']) { keyFound.push(1) }
-                    if (c == usersId.length ) { evalUsers(keyFound) }
-                    c++
-                })
-            }
+        client.lrange(query('users', 'id'), 0, -1, function(err, usersId) {
+             var keyFound = [];
+            if (usersId.length != 0) {
+                for(var i = 0, c = 1; i < usersId.length; i++) {
+                    client.hget([query('users', usersId[i]), 'name'], function(err, user) {
+                        if (user == data['name']) { keyFound.push(1) }
+                        if (c == usersId.length || usersId == null ) { evalUsers(keyFound) }
+                        c++
+                    })
+                }
+            } else { evalUsers(keyFound, 1) }
             function evalUsers(obj) {
-                console.log(arrayHasValue(obj, 1))
                 if (arrayHasValue(keyFound, 1)) {
-                    client.get('users', function(err, userCount) {
-                        createOrUpdateUser(data, userCount)
+                    client.get(query('users'), function(err, userCount) {
+                        createOrUpdateUser(data, userCount);
                         // return user
                     })
                 } else {
-                    client.get(queryConstruct('users'), function(err, userCount) {
-                        createOrUpdateUser(data, userCount)
+                    client.get(query('users'), function(err, userCount) {
+                        createOrUpdateUser(data, userCount);
                         //return user
-                        client.lpush(queryConstruct('users', 'id'), userCount)
-                        client.incr(queryConstruct('users'))
-                        client.lrange(queryConstruct('users', 'id'), 0, -1, redis.print)
+                        var updatingListsAndUsers = new Date();
+                        client.multi().lpush(query('users', 'id'), userCount)
+                            .incr(query('users'), redis.print)
+                            .lrange(query('users', 'id'), 0, -1, redis.print)
+                            .exec(function(err, reply) {
+                                console.log('Finish in ' + (new Date - updatingListsAndUsers) + ' ms')
+
+                                // benchmark here, dump all data
+                                client.lrange(query('users', 'id'), 0, -1, function(err, usersId) {
+                                    var start = new Date()
+                                    for(var i =0; i < usersId.length; i++) {
+                                        client.hgetall(query('users', usersId[i]), function(err, user) {
+                                            console.log(user)
+                                            if (i == usersId.length - 1) {
+                                                console.log('Pushed all in ' + (new Date() - start) + ' ms')
+                                            }
+
+                                        })
+                                    }
+                                })
+                            })
                     })
                 }
             }
         });
     })
-
 })
 
 function initializeUsers() {
-    client.get(queryConstruct('users'), function(err, reply) {
+    client.get(query('users'), function(err, reply) {
         if (reply == null) {
-            client.set(queryConstruct('users'), '0', redis.print)
+            client.set(query('users'), '0', redis.print)
         }
         console.log(reply)
     });
 }
 
-function queryConstruct() {
+function query() {
     var arr = [];
     for(var i = 0; i < arguments.length; i++) { arr.push(arguments[i]) }
     return arguments.length == 1 ? arr[0] : arr.join(':');
@@ -104,7 +123,10 @@ function arrayHasValue(arr, val) {
 function createOrUpdateUser(arr, userCount) {
     for(key in arr) {
         if(arr[key] != '') {
-            client.hset([queryConstruct('users', userCount), key, arr[key]], redis.print)
+            var start = new Date()
+            client.hset([query('users', userCount), key, arr[key]], function(err, reply) {
+                console.log('Reply ' + reply + ' : ' + (new Date() - start) + ' ms')
+            })
         }
     }
 }
