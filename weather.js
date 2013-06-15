b7cf76bfd9a4dc2b444f5734dbb873f9d3833b99
jeane paul <jeanepaul@gmail.com>
2013-06-16 04:41:00 +0900

    mod weather.js

--- weather.js
+++ weather.js
@@ -5,9 +5,9 @@ var loop_delay = 10000
 function getData() {
     http.get('http://api.openweathermap.org/data/2.5/weather?q=Akiruno-shi', function(response) {
         response.on('data', function(chunk) {
-            //console.log(JSON.parse(chunk).main)
-            var dataArr = JSON.parse(chunk).main
-            console.log(parseInfo(JSON.stringify(dataArr)))
+            console.log(JSON.parse(chunk))
+//            var dataArr = JSON.parse(chunk).main
+//            console.log(parseInfo(JSON.stringify(dataArr)))
 //            for(var i = 0; i < dataArr.length; i++) {
 //                console.log(JSON.parse(chunk).main[i])
 //                if(dataArr[i] == 'temp') {
