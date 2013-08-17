d84fde19ba82faad882e8f54b6d898172baae7b7
jeane paul <jeanepaul@gmail.com>
2013-08-17 20:48:12 +0900

    added redis

--- hackernews/hackernews.js
+++ hackernews/hackernews.js
@@ -4,31 +4,35 @@ var http = require('http')
     , css = require('term-css')
     , fs = require('fs')
     , style = fs.readFileSync('hackernews_style.css', 'utf8')
-    , counter = 0;
+    , counter = 0
+    , async = require('async')
+    , redisMaster = require('../testing/redisdb').configServer;
 
 
-setInterval(function() {
-    counter++;
-    var startTime = new Date();
-    http.get(' http://api.ihackernews.com/page', function(response) {
-        var data = '', obj;
-        response.on('data', function(chunk){
-            data += chunk;
-        });
-        response.on('end', function(){
-            if(myUtils.isJson(data)) {
-                var obj = JSON.parse(data);
-                var hackerNewsData = obj['items'];
-                for(var i = 0; i < Object.keys(hackerNewsData).length; i++) {
-                    var parseData = { id: i,  title: hackerNewsData[i]['title'], link: hackerNewsData[i]['url'] }
-                    var fn = css.compile('{id}: {title} \n    {link}', style)
-                    log(fn(parseData))
-                }
-                log('Took ' + (new Date() - startTime) + 'ms ' + 'counter ' + counter +' ----------------------------------------')
-            } else { log('empty ' + myUtils.timeString(new Date())) }
-        });
-
+function initialize() {
+    redisMaster.get('hackernews:interval', function(err, getReply) {
+        counter++;
+        var startTime = new Date();
+        http.get(' http://api.ihackernews.com/page', function(response) {
+            var data = '', obj;
+            response.on('data', function(chunk){
+                data += chunk;
+            });
+            response.on('end', function(){
+                if(myUtils.isJson(data)) {
+                    var obj = JSON.parse(data);
+                    var hackerNewsData = obj['items'];
+                    for(var i = 0; i < Object.keys(hackerNewsData).length; i++) {
+                        var parseData = { id: i,  title: hackerNewsData[i]['title'], link: hackerNewsData[i]['url'] }
+                        var fn = css.compile('{id}: {title} \n    {link}', style)
+                        log(fn(parseData))
+                    }
+                    log('Took ' + (new Date() - startTime) + 'ms ' + 'counter ' + counter +' ----------------------------------------')
+                } else { log('empty ' + myUtils.timeString(new Date())) }
+            });
+        })
+        setTimeout(initialize, getReply)
     })
-}, 3000)
+}
 
-
+initialize()
